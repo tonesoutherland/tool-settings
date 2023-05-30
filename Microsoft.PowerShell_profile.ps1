20866c2ace113244da411bafeb56b378d1cdebde
Tone Southerland <3523551+tonesoutherland@users.noreply.github.com>
2023-05-30 09:27:55 -0500

    update pwsh profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,6 @@
 Import-Module "C:\code\Kno2\builds\kno2.emulation.psm1" -force
 Import-Module posh-git
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/multiverse-neon.omp.json" | Invoke-Expression
+Set-alias kno2 C:\code\kno2-cli\src\Kno2.CLI\bin\Debug\net7.0\kno2.exe
+
+
